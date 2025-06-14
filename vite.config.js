48736dd23a038ff6f6c764e152ed90a17fa43368
Sartre150 <Immanuelsartre@gmail.com>
2025-06-13 21:13:52 -0600

    Corrige la ruta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PolifoniasdelAmar/', // ¡IMPORTANTE! Reemplaza esto con el nombre de tu repositorio
+  base: '/Polifonias-del-Amar/',
 })
