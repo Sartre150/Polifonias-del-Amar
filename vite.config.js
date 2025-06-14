bce0aef8fe55d84f623a4ccf531bdda37e272338
Sartre150 <Immanuelsartre@gmail.com>
2025-06-13 21:30:34 -0600

    usar carpeta "docs"

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Polifonias-del-Amar/',
+  build: {
+    outDir: 'docs'  // <-- AÑADE ESTA SECCIÓN
+  }
 })
